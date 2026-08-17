baa0505eac7f45ae8e03c182f3f5bfc9a5bf98cb
navneetsingh19 <navneet.singh.uiux.19@gmail.com>
2026-08-18 01:47:14 +0530

    initial push of practice

---
+++ practice.js
@@ -0,0 +1,11 @@
+let age =1 ;
+
+if (age < 18) {
+    console.log("You are a minor.");
+}
+else if (age >= 18 && age < 65) {
+    console.log("You are an adult.");
+}
+else {
+    console.log("You are a senior citizen.");
+}
